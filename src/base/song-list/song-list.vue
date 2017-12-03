44d716cf30fdf542707b067e620277ddd051fd9f
xingbang <suoxb@foxmail.com>
2017-12-03 22:31:51 +0800

    优化

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="song-list">
-    <li v-for="song in songs" class="item">
+    <li @click="selectItem(song,index)" v-for="(song,index) in songs" class="item">
       <div class="content">
         <h2 class="name">{{song.name}}</h2>
         <p class="desc">{{getDesc(song)}}</p>
@@ -19,6 +19,9 @@
     methods: {
       getDesc (song) {
         return `${song.singer}.${song.album}`
+      },
+      selectItem (item, index) {
+        this.$emit('select', item, index)
       }
     }
   }
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,12 @@
+import {playMode} from '@/common/js/config'
 const state = {
-  singer: {}
+  singer: {},
+  playing: false, // 播放或暂停
+  fullScreen: false,  // 全屏
+  playList: [], // 播放列表
+  sequenceList: [], // 播放列表
+  mode: playMode.sequence,  // 播放模式
+  currentIndex: -1 // 当前播放索引
 }
 
 export default state
